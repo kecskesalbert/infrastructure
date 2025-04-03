443ca3cc39bf79023714831cc4c4ebee90ff717b
Albert Kecskés <kecskesalbert@users.noreply.github.com>
2025-04-03 19:39:35 +0100

    Added Github action: pylint

---
+++ aws_lambda_swapcase/src/tests/test_invoke_noargs.py
@@ -0,0 +1,10 @@
+import sys
+sys.path.append( '..')
+from aws_lambda_swapcase.src.lambda_swapcase import lambda_handler
+
+"""
+    Return 400 when invoked without parameter
+"""
+def test_noargs():
+    res = lambda_handler({}, {})
+    assert res['statusCode'] == '400'
